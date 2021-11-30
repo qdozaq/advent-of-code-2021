5787a1c43e955df05ad18248c90f341dc9cef90f
Paul Mendoza <paul.mendoza@vimeo.com>
2021-11-30 18:09:11 -0500

    more setup

---
+++ advent/src/main.rs
@@ -0,0 +1,5 @@
+use day_1;
+
+fn main() {
+    day_1::run()
+}
--- day_1/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let c = add(1,2);
-    println!("Hello, world! {}", c);
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
