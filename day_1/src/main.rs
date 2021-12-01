6ed287c6f10c8f253c1534bdece20ee2307cf02c
Paul Mendoza <paul.mendoza@vimeo.com>
2021-12-01 11:19:06 -0500

    consolidate day

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,8 +1,14 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::u32::MAX;
+use std::collections::VecDeque;
 
 fn main() {
+    star_one();
+    star_two();
+}
+
+fn star_one () {
     let reader = BufReader::new(File::open("input.txt").expect("Cannot open"));
 
     let mut count = 0;
@@ -18,6 +24,35 @@ fn main() {
         }
 
         prev = num;
+    }
+
+    println!("{}", count);    
+}
+
+fn star_two () {
+    let reader = BufReader::new(File::open("input.txt").expect("Cannot open"));
+
+    let mut count = 0;
+    let mut prev: u32 = MAX;
+    let mut queue = VecDeque::with_capacity(3);
+    for line in reader.lines() {
+        let num: u32 = match line.unwrap().parse() {
+            Ok(num) => num,
+            Err(_) => panic!("NaN")
+        };
+
+        queue.push_back(num);
+
+        if queue.len() >= 3{
+            let num = queue.iter().sum();
+            if num > prev {
+                count = count + 1;
+            }
+            prev = num;
+
+            queue.pop_front();
+        }
+
     }
 
     println!("{}", count);    
