7a7cc36b09a8ca733f0aa7d140d202fd17b8f033
Paul Mendoza <doza@indiginis.com>
2021-12-05 15:41:57 -0500

    day 4

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,17 +1,23 @@
 use std::fmt::Result;
 
-const WIDTH: usize = 5;
+const SIZE: usize = 5;
+
+#[derive(Debug)]
 struct Board {
     numbers: Vec<isize>,
+    bingo: bool,
 }
 
 impl Board {
     fn new(numbers: Vec<isize>) -> Board {
-        Board { numbers }
+        Board {
+            numbers,
+            bingo: false,
+        }
     }
-    fn callNum(&mut self, called_number: &isize) -> bool {
+    fn callNum(&mut self, called_number: isize) -> bool {
         for num in self.numbers.iter_mut() {
-            if num.eq(&called_number) {
+            if (*num).eq(&called_number) {
                 *num = -1;
             }
         }
@@ -19,29 +25,73 @@ impl Board {
         self.check()
     }
 
-    fn check(&self) -> bool {
+    fn check(&mut self) -> bool {
         // check rows
-        for col in 0..WIDTH {}
+        for col in 0..SIZE {
+            let mut check = 0;
+            for row in 0..SIZE {
+                if self.numbers[SIZE * row + col] == -1 {
+                    check += 1;
+                }
+            }
+            if check == SIZE {
+                self.bingo = true;
+                return true;
+            }
+        }
 
-        true
+        for row in 0..SIZE {
+            let mut check = 0;
+            for col in 0..SIZE {
+                if self.numbers[SIZE * row + col] == -1 {
+                    check += 1;
+                }
+            }
+            if check == SIZE {
+                self.bingo = true;
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 fn main() {
-    let numbers: Vec<isize> = include_str!("../test_nums.txt")
+    let numbers: Vec<isize> = include_str!("../nums.txt")
         .to_string()
         .split(",")
         .map(|n| n.parse::<isize>().unwrap())
         .collect();
 
-    let boards_text = include_str!("../test_boards.txt");
-    let boards = boards_text
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| n.parse::<isize>().unwrap())
-                .collect::<Vec<_>>()
+    let boards_text = include_str!("../boards.txt");
+    let mut boards = boards_text
+        .split("\n\n")
+        .map(|section| {
+            // map sections of numbers to a board
+            let board_nums = section
+                .lines()
+                .map(|line| {
+                    let mut line = line.chars().filter(|l| *l != '\n').collect::<String>();
+                    line.split_ascii_whitespace()
+                        .map(|n| n.parse::<isize>().unwrap())
+                        .collect::<Vec<isize>>()
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+            Board::new(board_nums)
         })
         .collect::<Vec<_>>();
 
-    println!("{:?}", boards);
+    'outer: for n in numbers {
+        for board in boards.iter_mut() {
+            if board.callNum(n) {
+                let score: isize = board.numbers.iter().filter(|n| **n >= 0).sum();
+                let score = score * n;
+                println!("{}", score);
+                break 'outer;
+            }
+        }
+    }
+
+    // println!("{:?}", boards);
 }
